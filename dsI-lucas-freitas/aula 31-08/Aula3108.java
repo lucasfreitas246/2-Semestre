5ed9f447da335f5290b0af50ea9d6e74dc63df37
Lucas Costa <lukasfreitas246@gmail.com>
2026-08-31 22:20:36 -0300

    att

---
+++ dsI-lucas-freitas/aula 31-08/Aula3108.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula31.pkg08;
+
+/**
+ *
+ * @author Aluno CA
+ */
+public class Aula3108 
+{public static void main (String[]args)
+    {int r, n=0;
+     if ((n>2)&&(n<0))
+     r=(n+1)*(n-2);
+    
+
+else{
+     r=(n+2)*(n-1);
+
+     }
+                   System.out.println("Resultado: "+r);
+
+    }
+}
+    
+
